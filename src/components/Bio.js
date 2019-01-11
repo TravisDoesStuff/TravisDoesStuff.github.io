a075029d008ce9d5327ebef4b3a59a1532b351f0
tnburt <travisnburt@gmail.com>
2019-01-11 09:59:46 -0700

    Fix widths

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Person from '../data/Person';
 import Links from '../data/Links';
 import { Link } from 'react-router-dom';
-import { Header, Grid, Image, Icon, Button, Divider, Responsive } from 'semantic-ui-react';
+import { Header, Grid, Image, Icon, Button, Divider, Responsive, Container } from 'semantic-ui-react';
 import '../css/bio.css';
 
 import Skills from './Skills';
@@ -72,66 +72,68 @@ class Bio extends Component {
         return (
             <div className='about-section'>
                 <Divider />
-                <div className='about-block about-me'>
+                <Container className='about-me'>
                     <Header as='h2'>About me</Header>
                     <div className='aboutParagraph'>
                         { person.description.map((paragraph,p) => 
                             <p key={p}>{ paragraph }</p>
                         )}
                     </div>
-                </div>
+                </Container>
                 <Divider />
-                <div className='about-block'>
+                <Container>
                     <Skills skills={ person.skills } />
-                </div>
+                </Container>
                 <Divider />
-                <div className='about-block about-timeline'>
+                <Container>
                     <Timeline timeline={ person.history } />
-                </div>
+                </Container>
             </div>
         )
     }
 
     renderActions(links) {
         return (
-            <div className='about-section action-section about-block'>
-                <Responsive maxWidth={Responsive.onlyComputer.minWidth} className='action-row' >
-                    <Grid columns={2}>
-                        <Grid.Row>
-                            <Grid.Column>
-                                { links.map((link) =>
-                                    <Button circular icon key={ link._id } href={ link.link } target='_blank' size='large' color={ link.color }>
-                                        <Icon link name={ link.icon } />
+            <div className='about-section action-section'>
+                <Container>
+                    <Responsive maxWidth={Responsive.onlyComputer.minWidth} className='action-row about-section' >
+                        <Grid columns={2}>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    { links.map((link) =>
+                                        <Button circular icon key={ link._id } href={ link.link } target='_blank' size='large' color={ link.color }>
+                                            <Icon link name={ link.icon } />
+                                        </Button>
+                                    )}
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Button color='blue' icon labelPosition='right' as={ Link } to={ '/projects' } size='large' fluid style={{ fontWeight: 'bold' }}>
+                                        Projects
+                                        <Icon name='angle right' />
                                     </Button>
-                                )}
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Button color='blue' icon labelPosition='right' as={ Link } to={ '/projects' } size='large' fluid style={{ fontWeight: 'bold' }}>
-                                    Projects
-                                    <Icon name='angle right' />
-                                </Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Responsive>
-                <Responsive minWidth={Responsive.onlyComputer.minWidth} className='action-row' >
-                    <Grid columns={2}>
-                        <Grid.Row>
-                            <Grid.Column>
-                                { links.map((link) =>
-                                    <Button circular icon key={ link._id } href={ link.link } target='_blank' size='large' style={{ fontWeight: 'bold' }} color={ link.color }>
-                                        <Icon link name={ link.icon } /> { link.name }
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Responsive>
+                    <Responsive minWidth={Responsive.onlyComputer.minWidth} className='action-row' >
+                        <Grid columns={2}>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    { links.map((link) =>
+                                        <Button circular icon key={ link._id } href={ link.link } target='_blank' size='large' style={{ fontWeight: 'bold' }} color={ link.color }>
+                                            <Icon link name={ link.icon } /> { link.name }
+                                        </Button>
+                                    )}
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Button color='blue' icon labelPosition='right' as={ Link } to={ '/projects' } size='large' fluid style={{ fontWeight: 'bold' }}>
+                                        View my Projects <Icon name='angle right' />
                                     </Button>
-                                )}
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Button color='blue' icon labelPosition='right' as={ Link } to={ '/projects' } size='large' fluid style={{ fontWeight: 'bold' }}>
-                                    View my Projects <Icon name='angle right' />
-                                </Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Responsive>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Responsive>
+                </Container>
             </div>
         )
     }
