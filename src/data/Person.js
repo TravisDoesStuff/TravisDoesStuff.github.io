a8c39966b0d34ea5e3b3844e4a9ece987dc1c422
tnburt <travisnburt@gmail.com>
2020-02-19 21:53:01 -0700

    Slight tweak to grammar in descriptions

--- src/data/Person.js
+++ src/data/Person.js
@@ -2,10 +2,10 @@ const Person = {
     name: 'Travis Burt',
     title: 'Developer, Designer & Stuff',
     description: [
-        "Technology has always been at the front of enhancing the human experience. As people become more connected, information is easier than ever to access and it is important for interfaces to be intuitive for users to find what they are looking for.",
+        "Technology has always been at the front of enhancing the human experience. As people become more connected, information is easier than ever to access and interfaces need to be intuitive for users to find what they are looking for.",
         "Inspiration for my career in software development first came about from folks modifying computer games from seriousness to something silly. I wanted to try something similar and started to browse and modify sound-bytes within my favorite games to make them how I want. As time went on I delved into modifying graphics, models, and eventually learned about programming.",
-        "So far in my career I have been handling large data sets to present users how they are recovering following a medical procedure, or showing how utility customers are reducing their carbon footprint over time. Overall my passion is providing information to people to learn from and help with decision making, software development is the perfect means of doing so in a connected world.",
-        "Outside of development I have been exploring virtual worlds through VR gaming; although I still haven’t forgotten about the real-world (yet) and equally enjoy explore local places and getting to know folks all around both inside and outside the software development industry. I also enjoy expressing my creative side through computer graphics, 3D modeling, and drawing...anything that brings thoughts to life."
+        "So far in my career, I have been handling large data sets to present users how they are recovering following a medical procedure; or showing how utility customers are reducing their carbon footprint over time. Overall my passion is providing information to people to learn from and help with decision making, software development is the perfect means of doing so in a connected world.",
+        "Outside of development, I have been exploring virtual worlds through VR gaming; although I still haven’t forgotten about the real-world (yet) and equally enjoy explore local places and getting to know folks all around both inside and outside the software development industry. I also enjoy expressing my creative side through computer graphics, 3D modeling, and drawing...anything that brings thoughts to life."
     ],
     skills: [
         {
@@ -63,9 +63,9 @@ const Person = {
             icon: 'briefcase',
             description: 'Franklin Energy integrates with utility companies to provide advice to both residential and commercial customers on energy savings. Through self-reporting surveys regarding their building and lifestyle, tips are provided on actions customers can perform to save more financially, reduce carbon emissions, and improve local health.',
             tasks: [
-                'Continuously update user-interface to ensure that customers understand their data in order to make informed decisions.',
+                'Continuously update user-interface to ensure that customers understand their data to make informed decisions.',
                 'Accelerated massive data imports through code optimization and data refining scripts.',
-                'Directly communicated with clients in order to understand requirements at both technical and design levels.',
+                'Directly communicate with clients in order to understand requirements at both technical and design levels.',
             ]
         },
         {
@@ -75,10 +75,10 @@ const Person = {
             icon: 'briefcase',
             description: 'Oberd is a healthcare based application primarily focused on outcomes, or how a patient benefits in their life after a medical procedure. Data management tools are also provided to clinics to schedule appointments and conduct studies.',
             tasks: [
-                'Developed product activation and administration tool with React. Account management turnaround times reduced from hours to minutes and reduced client tasks for development team.',
-                'Took ownership of Patient Satisfaction product, optimization of CakePHP code and database queries reduced report time from minutes to seconds, enhancing client retention and product sales.',
+                'Developed product activation and administration tool with React. Account management turnaround times reduced from hours to minutes and reduced client tasks for the development team.',
+                'Took ownership of Patient Satisfaction product; optimization of CakePHP code and database queries reduced report time from minutes to seconds, enhancing client retention and product sales.',
                 'Converted backend data management system to Lumen-based RESTful API and AWS S3.',
-                'Collaborated with developers to establish coding practices to improve maintainability of code-base.',
+                'Collaborated with developers to establish coding practices to improve the maintainability of code-base.',
                 'Maintained clinical management application, utilizing a Kanban system to add requested features and resolve defects; continuous deployment through Codeship to Amazon Web Services (AWS).'
             ]
         },
